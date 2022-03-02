8e39eec50ba20ecce4418d65bbdd043ec3653c63
D99ZY <dtriegaardt@outlook.com>
2022-03-02 15:10:09 +0000

    Add func to render all StudentTile in array

--- react-student-app/src/components/StudentList.tsx
+++ react-student-app/src/components/StudentList.tsx
@@ -4,22 +4,26 @@ import styles from './StudentList.module.css';
 
 
 const StudentList = () => {
+
+    const displayStudents = () => {
+        return student_data.Students.map((student) => (
+            <StudentTile
+                key={student.source_id}
+                firstName={student.first_name}
+                lastName={student.last_name}
+                schoolYear={student.year_code}
+                gender={student.gender}
+                dob={student.dob.split(" ")[0]}
+            />
+        ));
+    };
+
     return (
         <div className={styles.container}>
             <h3>StudentList</h3>
 
-            <StudentTile
-                key={student_data.Students[0].source_id}
-                firstName={student_data.Students[0].first_name}
-                lastName={student_data.Students[0].last_name}
-                schoolYear={student_data.Students[0].year_code}
-            />
-            <StudentTile
-                key={student_data.Students[1].source_id}
-                firstName={student_data.Students[1].first_name}
-                lastName={student_data.Students[1].last_name}
-                schoolYear={student_data.Students[1].year_code}
-            />
+            {displayStudents()}
+            
         </div>
     );
 };
