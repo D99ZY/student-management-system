af3a06b73a20ec662c3c21a37fe77c779d216da2
D99ZY <dtriegaardt@outlook.com>
2022-03-02 20:57:03 +0000

    Add input form to update global state

--- react-student-app/src/components/Search.tsx
+++ react-student-app/src/components/Search.tsx
@@ -1,10 +1,41 @@
-// import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { updateOptions } from '../context/actions';
 import styles from './Search.module.css';
 
 const Search = () => {
+
+    // Context
+    const {
+        state: { options }, dispatch
+    } = useContext(GlobalContext);
+
+    // State
+    const [searchState, setSearchState] = useState<string>('');
+
+    // Set searchState on input change
+    const handleOnChange = (e: React.FormEvent<HTMLInputElement>): void => {
+        setSearchState(e.currentTarget.value);
+    }
+
+    // Update context on searchState change
+    useEffect(() => {
+        let newOptions = options;
+        newOptions.search = searchState;
+        dispatch(updateOptions(newOptions));
+    }, [dispatch, searchState, options]);
+
+    // JSX
     return (
         <div className={styles.container}>
-            <h4>Search</h4>
+            <form className={styles.inputForm}>
+                <input 
+                    onChange={handleOnChange}
+                    placeholder='Search...'
+                    type='text'
+                    value={searchState}
+                />
+            </form>
         </div>
     );
 };
