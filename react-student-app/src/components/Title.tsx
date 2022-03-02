7f6bb4d54cd379913151782b5572f2c5ff04f234
D99ZY <dtriegaardt@outlook.com>
2022-03-02 23:51:11 +0000

    Add conditional rendering to Title

--- react-student-app/src/components/Title.tsx
+++ react-student-app/src/components/Title.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 import styles from './Title.module.css';
 
 interface TitleProps {
@@ -5,9 +7,16 @@ interface TitleProps {
 }
 
 const Title = ({title}: TitleProps) => {
+
+    // Context
+    const {
+        state: { student }
+    } = useContext(GlobalContext);
+
+    // JSX
     return (
         <h1 className={styles.container}>
-            {title}
+            {student.displayStudent ? `${student.firstName} ${student.lastName}` : title}
         </h1>
     );
 };
