db4d999b95d16cb942c9186964346d0525ae1c2c
D99ZY <dtriegaardt@outlook.com>
2022-03-02 13:54:42 +0000

    Add ViewBar component

---
+++ react-student-app/src/components/ViewBar.tsx
@@ -0,0 +1,11 @@
+import styles from './ViewBar.module.css';
+
+const ViewBar = () => {
+    return (
+        <div className={styles.container}>
+            <h2>ViewBar</h2>
+        </div>
+    );
+};
+
+export default ViewBar;
