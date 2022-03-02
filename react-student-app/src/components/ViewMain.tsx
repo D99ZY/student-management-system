09a3ff4343a62ce295b42104f650df66cdbc9fe7
D99ZY <dtriegaardt@outlook.com>
2022-03-02 23:51:46 +0000

    Add conditional rendering to ViewMain

--- react-student-app/src/components/ViewMain.tsx
+++ react-student-app/src/components/ViewMain.tsx
@@ -1,10 +1,30 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import ViewBar from './ViewBar';
 import StudentList from './StudentList';
+import StudentCard from './StudentCard';
 import styles from './ViewMain.module.css';
 
 const ViewMain = () => {
+
+    // Context
+    const {
+        state: { student }
+    } = useContext(GlobalContext);
+
+
     return (
         <div className={styles.container}>
-            <StudentList />
+            {student.displayStudent ? 
+                <div>
+                    <StudentCard />
+                </div>
+                : 
+                <div>
+                    <ViewBar />
+                    <StudentList />
+                </div>
+            }
         </div>
     );
 };
