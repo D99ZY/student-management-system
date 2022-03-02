857ca693a4b0cb423e25c35332edff03c69d6ffc
D99ZY <dtriegaardt@outlook.com>
2022-03-02 20:58:05 +0000

    Add sort, filter, and search functionality

--- react-student-app/src/components/StudentList.tsx
+++ react-student-app/src/components/StudentList.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 import StudentTile from './StudentTile';
 import student_data from '../student_records.json';
 import styles from './StudentList.module.css';
@@ -5,23 +7,73 @@ import styles from './StudentList.module.css';
 
 const StudentList = () => {
 
-    const displayStudents = () => {
-        return student_data.Students.map((student) => (
-            <StudentTile
-                key={student.source_id}
-                firstName={student.first_name}
-                lastName={student.last_name}
-                schoolYear={student.year_code}
-                gender={student.gender}
-                dob={student.dob.split(" ")[0]}
-            />
-        ));
+    // Context
+    const {
+        state: { options }
+    } = useContext(GlobalContext);
+
+    // Display students function
+    const displayStudents = (sort: number, filter: number, search: string) => {
+
+        // Local variables
+        let studentArray = student_data.Students;
+        let sortedArray, filteredArray, searchedArray;
+
+        // Sort
+        if (sort === 0) {
+            sortedArray = studentArray.sort((a, b) => a.first_name.localeCompare(b.first_name));
+        }
+        else {
+            sortedArray = studentArray.sort((a, b) => a.last_name.localeCompare(b.last_name));
+        }
+
+        // Filter
+        if (filter > 0) {
+            filteredArray = sortedArray
+                .filter(({ year_code }) => {
+                    return parseInt(year_code) === filter;
+                });
+        }
+        else {
+            filteredArray = studentArray;
+        }
+
+        // Search
+        if (search !== '') {
+            searchedArray = filteredArray
+                .filter(({ first_name, last_name }) => {
+                    return (first_name.toLowerCase().includes(search.toLowerCase()) || 
+                        last_name.toLowerCase().includes(search.toLowerCase()));
+                });
+        }
+        else {
+            searchedArray = filteredArray;
+        }
+
+        // Return
+        if (searchedArray.length > 0) {
+            return searchedArray.map((student) => (
+            
+                <StudentTile
+                    key={student.source_id}
+                    firstName={student.first_name}
+                    lastName={student.last_name}
+                    schoolYear={student.year_code}
+                    gender={student.gender}
+                    dob={student.dob.split(" ")[0]}
+                />
+            ));
+        }
+        else {
+            return <h2 className={styles.none}>No students found...</h2>;
+        }
     };
 
+    // JSX
     return (
         <div className={styles.container}>
 
-            {displayStudents()}
+            {displayStudents(options.sort, options.filter, options.search)}
 
         </div>
     );
