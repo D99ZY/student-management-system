ab0a728c8646ec39b86ee9e6b68c21dabb992bc5
D99ZY <dtriegaardt@outlook.com>
2022-03-02 14:30:43 +0000

    Render StudentTile with data from student_records

--- react-student-app/src/components/StudentList.tsx
+++ react-student-app/src/components/StudentList.tsx
@@ -1,4 +1,5 @@
 import StudentTile from './StudentTile';
+import student_data from '../student_records.json';
 import styles from './StudentList.module.css';
 
 
@@ -6,7 +7,19 @@ const StudentList = () => {
     return (
         <div className={styles.container}>
             <h3>StudentList</h3>
-            <StudentTile />
+
+            <StudentTile
+                key={student_data.Students[0].source_id}
+                firstName={student_data.Students[0].first_name}
+                lastName={student_data.Students[0].last_name}
+                schoolYear={student_data.Students[0].year_code}
+            />
+            <StudentTile
+                key={student_data.Students[1].source_id}
+                firstName={student_data.Students[1].first_name}
+                lastName={student_data.Students[1].last_name}
+                schoolYear={student_data.Students[1].year_code}
+            />
         </div>
     );
 };
