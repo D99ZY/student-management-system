db5dd128bd319fc9666b10cc57b1d017283d768e
D99ZY <dtriegaardt@outlook.com>
2022-03-02 13:21:34 +0000

    Initial commit

---
+++ react-student-app/src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './App.module.css';
+import './globals.css';
+
+function App() {
+    return (
+        <div className={styles.app}>
+            <h1>Hello, World!</h1>
+        </div>
+    );
+}
+
+export default App;
