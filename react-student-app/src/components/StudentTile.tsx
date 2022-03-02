a8a71f649cb4fcfaaa8c619492fa399b95b9c0db
D99ZY <dtriegaardt@outlook.com>
2022-03-02 14:13:53 +0000

    Add StudentTile component

---
+++ react-student-app/src/components/StudentTile.tsx
@@ -0,0 +1,11 @@
+import styles from './StudentTile.module.css';
+
+const StudentTile = () => {
+    return (
+        <div className={styles.container}>
+            <h4>StudentTile</h4>
+        </div>
+    );
+};
+
+export default StudentTile;
