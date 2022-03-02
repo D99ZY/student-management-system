367644fb6a01b23fe2ab52ae30d4f6968aba004e
D99ZY <dtriegaardt@outlook.com>
2022-03-02 15:06:51 +0000

    Add gender and dob

--- react-student-app/src/components/StudentTile.tsx
+++ react-student-app/src/components/StudentTile.tsx
@@ -4,6 +4,8 @@ interface StudentInfo {
     firstName: string;
     lastName: string;
     schoolYear: string;
+    gender: string;
+    dob: string;
 }
 
 const StudentTile = (props: StudentInfo) => {
@@ -11,6 +13,10 @@ const StudentTile = (props: StudentInfo) => {
         <div className={styles.container}>
             <h2>{`${props.firstName} ${props.lastName}`}</h2>
             <h3>{`Year: ${props.schoolYear}`}</h3>
+            <div className={styles.bottom}>
+                <h4>{`Gender: ${props.gender}`}</h4>
+                <h4>{`DOB: ${props.dob}`}</h4>
+            </div>
         </div>
     );
 };
