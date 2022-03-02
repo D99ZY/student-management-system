3258cafc46d88fd87c92c6d0555b0945094d6f6b
D99ZY <dtriegaardt@outlook.com>
2022-03-02 13:54:58 +0000

    Add ViewMain component

---
+++ react-student-app/src/components/ViewMain.tsx
@@ -0,0 +1,11 @@
+import styles from './ViewMain.module.css';
+
+const ViewMain = () => {
+    return (
+        <div className={styles.container}>
+            <h2>ViewMain</h2>
+        </div>
+    );
+};
+
+export default ViewMain;
