dadcfd624e1a88ccca70efcbf288a3bf714a1356
D99ZY <dtriegaardt@outlook.com>
2022-03-02 13:56:26 +0000

    Add ViewBar and ViewMain to App

--- react-student-app/src/App.tsx
+++ react-student-app/src/App.tsx
@@ -1,11 +1,22 @@
 import Title from './components/Title';
+import ViewBar from './components/ViewBar';
+import ViewMain from './components/ViewMain';
 import styles from './App.module.css';
 import './globals.css';
 
 function App() {
     return (
         <div className={styles.app}>
-            <Title title='My Students' />
+
+            <div className={styles.titleContainer}>
+                <Title title='My Students' />
+            </div>
+
+            <div className={styles.appContainer}>
+                <ViewBar />
+                <ViewMain />
+            </div>
+
         </div>
     );
 }
